c0c167dac53d23b5de9761475170073915145b5b
yogi.cahyono@grabtaxi.com <yogi.cahyono@grabtaxi.com>
2019-02-13 18:44:20 +0700

    chunks function

--- queryGrab/__init__.py
+++ queryGrab/__init__.py
@@ -2,8 +2,14 @@ from .Presto import queryPr, pushPr
 from .geoConverter import convert_polygon_to_geohash, to_geohash
 from .GoogleSheets import readGs, writeGs, shareGs, createGs, deleteGs, createFolder, moveFile
 
-__version__ = '2.1.3'
+__version__ = '2.1.4'
 
 def sample(N,e):
 	from math import ceil
 	return int(ceil(N*1.0/(1+N*e*e)))
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
